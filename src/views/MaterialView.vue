508f19281b3a53efbbeee5f0f82e46001f787aab
ThIsJaCk <y23657689@gmail.com>
2023-09-15 20:09:25 +0800

    222

--- src/views/MaterialView.vue
+++ src/views/MaterialView.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-<h1>
-    MaterialView
-</h1>
+<div class="w-hull h-full">
+    <img src="@\assets\images\temp\menu.png" alt="" class="object-cover w-full h-full">
+</div>
 </template>
 
 <style scoped>
