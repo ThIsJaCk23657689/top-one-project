ba8111c6c02cca4b5477b7f40aff1fbc8bdfc25c
ThIsJaCk <y23657689@gmail.com>
2024-02-01 02:37:14 +0800

    20240201

--- src/views/Structure/FirstFloorView.vue
+++ src/views/Structure/FirstFloorView.vue
@@ -2,26 +2,26 @@
 import FloorPlanWrapper from '@/layouts/FloorPlanWrapper.vue';
 import RouteButton from '@/components/RouteButton.vue';
 
-const mainImage = new URL('@/assets/images/floor-plans/1F.svg', import.meta.url).href;
+const mainImage = new URL('@/assets/images/floor-plans/1F.jpg', import.meta.url).href;
 const titleImage = new URL('@/assets/images/floor-plans/1F-title.svg', import.meta.url).href;
 
 const spots = [
-    { marker: 'A', top: 61.5, left: 62.3, text: '林蔭大道', url: '/structure/facilities/frontdoor' },
-    { marker: 'B', top: 45.7, left: 62.2, text: '迎賓大廳', url: '/structure/facilities/lobby' },
-    { marker: 'C', top: 41, left: 69.4, text: '資源回收室', url: '#' },
-    { marker: 'D', top: 37.58, left: 56.5, text: '宅配室', url: '#' },
-    { marker: 'E', top: 30.89, left: 53.25, text: '信箱區', url: '/structure/facilities/mailbox' },
-    { marker: 'F', top: 19.4, left: 54.9, text: '閱覽室', url: '/structure/facilities/reading' },
-    { marker: 'G', top: 22.4, left: 65.36, text: '家教室', url: '#' },
-    { marker: 'H', top: 16.7, left: 65.3, text: '琴韻室', url: '#' },
-    { marker: 'I', top: 31.6, left: 45.4, text: '梯廳', url: '/structure/facilities/stair' },
-    { marker: 'J', top: 21, left: 46.5, text: '多功能交誼廳', url: '/structure/facilities/lounge' },
-    { marker: 'K', top: 38, left: 28.5, text: '活力健身房', url: '/structure/facilities/gym' },
-    { marker: 'L', top: 27.8, left: 28.45, text: '瑜珈教室', url: '/structure/facilities/yoga' },
-    { marker: 'M', top: 18.5, left: 30, text: '風鈴秘境', url: '/structure/facilities/oasis' },
-    { marker: 'N', top: 9.3, left: 61.6, text: '遊憩綠廊', url: '#' },
-    { marker: 'O', top: 13.6, left: 74.56, text: '大樹休憩平台', url: '/structure/facilities/relax' },
-    { marker: 'P', top: 36, left: 75.6, text: '社區車道', url: '#' },
+    { marker: 'A', top: 77.20, left: 65.00, text: '林蔭大道', url: '/structure/facilities/frontdoor' },
+    { marker: 'B', top: 58.00, left: 65.00, text: '迎賓大廳', url: '/structure/facilities/lobby' },
+    { marker: 'C', top: 52.00, left: 73.85, text: '資源回收室', url: '#' },
+    { marker: 'D', top: 47.86, left: 58.20, text: '宅配室', url: '#' },
+    { marker: 'E', top: 39.20, left: 53.70, text: '信箱區', url: '/structure/facilities/mailbox' },
+    { marker: 'F', top: 25.22, left: 55.98, text: '閱覽室', url: '/structure/facilities/reading' },
+    { marker: 'G', top: 29.12, left: 68.97, text: '家教室', url: '#' },
+    { marker: 'H', top: 21.71, left: 68.89, text: '琴韻室', url: '#' },
+    { marker: 'I', top: 40.50, left: 44.02, text: '梯廳', url: '/structure/facilities/stair' },
+    { marker: 'J', top: 27.30, left: 45.45, text: '多功能交誼廳', url: '/structure/facilities/lounge' },
+    { marker: 'K', top: 48.60, left: 23.05, text: '活力健身房', url: '/structure/facilities/gym' },
+    { marker: 'L', top: 35.40, left: 22.96, text: '瑜珈教室', url: '/structure/facilities/yoga' },
+    { marker: 'M', top: 24.05, left: 25.00, text: '風鈴秘境', url: '/structure/facilities/oasis' },
+    { marker: 'N', top: 12.40, left: 69.08, text: '遊憩綠廊', url: '#' },
+    { marker: 'O', top: 18.00, left: 80.10, text: '大樹休憩平台', url: '/structure/facilities/relax' },
+    { marker: 'P', top: 45.80, left: 81.28, text: '社區車道', url: '#' },
 ];
 
 </script>
